b3b92b1d8b53e43ff3dca4bfd4d74eecbbfee1e2
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-10-11 17:03:00 +0200

    In this video, we learned how to incorporate UIKit views into our project. We started by adding a UITextField to the screen. Next, we explored the process of sending data from UIKit to our SwiftUI application, and this was achieved through the use of a coordinator.
    Subsequently, we delved into the reverse process—sending data from our SwiftUI app back to our UIKit view. This was accomplished using the updateUIView function.
    
    Lastly, we explored a few ways to customize our UIKit view. We can create a custom initializer, allowing us to pass in custom data. Additionally, we can enhance our view by adding specific functions to manipulate data within our UIViewRepresentable after its initialization.

--- MasterSwiftUI/Applications/UIViewRepresentableBootCamp.swift
+++ MasterSwiftUI/Applications/UIViewRepresentableBootCamp.swift
@@ -8,11 +8,105 @@
 import SwiftUI
 
 struct UIViewRepresentableBootCamp: View {
+    @State var someTextHere:String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text(someTextHere)
+                .padding()
+                .font(.title)
+            Text("UIKit UIView only with background red.")
+            BasicUIPreviewPresentable()
+                .frame(height: 50)
+            Text("SwiftUI only with placeholder")
+            TextField("PlaceHolder...", text: $someTextHere)
+                .frame(height: 50)
+                .background(Color.blue)
+            Text("UIKit UITextField only with background blue.")
+            UITextFieldRepresentable(text: $someTextHere)
+                .updatePlaceHolder("You can type here . ..")
+                .frame(height: 50)
+                .background(Color.blue)
+        }
     }
 }
 
+// The way to Preview The code
+// PreviewProvider instead you can tell compiler by using #Preview
+//struct UIViewRepresentableBootCamp_Preview: PreviewProvider {
+//    static var previews: some View {
+//        UIViewRepresentableBootCamp()
+//    }
+//}
+
 #Preview {
     UIViewRepresentableBootCamp()
+}
+
+
+struct BasicUIPreviewPresentable: UIViewRepresentable {
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    
+}
+
+struct UITextFieldRepresentable: UIViewRepresentable {
+    @Binding var text:String
+    var placeHolderText:String
+    let placeHolderColor:UIColor
+    
+    init(text: Binding<String>, placeHolderText: String = "Default place holder...", placeHolderColor: UIColor = .red) {
+        self._text = text
+        self.placeHolderText = placeHolderText
+        self.placeHolderColor = placeHolderColor
+    }
+    
+    
+    func makeUIView(context: Context) -> UITextField {
+        let textField = getTextField()
+        textField.delegate = context.coordinator
+        
+        return textField
+    }
+    // transfer data From SwiftUI to UIKit
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = self.text
+    }
+    
+    
+    func updatePlaceHolder(_ text:String) -> UITextFieldRepresentable {
+        var textfieldRepresentable = self
+        textfieldRepresentable.placeHolderText = text
+        
+        return textfieldRepresentable
+    }
+    func getTextField()->UITextField {
+        let textField = UITextField(frame: .zero)
+        
+        let placeHolder = NSAttributedString(string: placeHolderText, attributes: [.foregroundColor: placeHolderColor ])
+        
+        textField.attributedPlaceholder = placeHolder
+        return textField
+    }
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(text: $text)
+    }
+    // transfer data From UIKit to SwiftUI
+    class Coordinator: NSObject, UITextFieldDelegate {
+        @Binding var text:String
+        
+        init(text: Binding<String>) {
+            self._text = text
+        }
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            self.text = textField.text ?? ""
+        }
+    }
 }
