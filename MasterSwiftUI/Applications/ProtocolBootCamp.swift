55d7065e7dc51abd6259c3aaeb8645c1e0d38bed
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-10-27 17:34:51 +0200

    using protocols in development to ensure that we have standard variables and funcitons for our development.

---
+++ MasterSwiftUI/Applications/ProtocolBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  ProtocolBootCamp.swift
+//  MasterSwiftUI
+//
+//  Created by Omid Shojaeian Zanjani on 27/10/23.
+//
+
+import SwiftUI
+
+struct ProtocolBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProtocolBootCamp()
+}
