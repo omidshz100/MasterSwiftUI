f825b9b0a7c5a8f2c52769c221b39aa404eed4a2
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-11-09 11:14:49 +0100

    I used Protocol in SwiftUI to make more standard inside applications

--- MasterSwiftUI/Applications/ProtocolBootCamp.swift
+++ MasterSwiftUI/Applications/ProtocolBootCamp.swift
@@ -7,22 +7,81 @@
 
 import SwiftUI
 // here we are going to use Protocols in our development
-protocol TextProtocol{
-    var text:String { get }
-}
-class TextDataSource: TextProtocol {
-    var text: String = "Hello world"
-}
-class TextDatasourceCustom: TextProtocol {
-    var text: String = "Hello world"
+struct DefaultColorThem : ColorThemProtocol{
+    let primary:Color = .blue
+    let secondary:Color = .white
+    let tertiary:Color = .gray
 }
 
+struct AlternativeColorThem: ColorThemProtocol{
+    let primary:Color = .red
+    let secondary:Color = .orange
+    let tertiary:Color = .green
+}
+
+struct AnotherColorThem:ColorThemProtocol{
+    var primary: Color = .blue
+    var secondary: Color = .red
+    var tertiary: Color = .purple
+}
+
+protocol ColorThemProtocol{
+    var primary:Color {get}
+    var secondary:Color {get}
+    var tertiary:Color {get}
+}
+
+class DefaultDataSource:ButtonDataSourceProtocol{
+    func buttonPressed() {
+        print("Button just pressed ....")
+    }
+    
+    var buttonText:String = "Protocols are awsome"
+}
+class AlternativeDataSource:ButtonTextProtocol{
+    var buttonText:String = "Protocols are awsome and UseFull ... "
+}
+protocol ButtonTextProtocol{
+    var buttonText:String {get}
+}
+protocol ButtonPressedProtocol{
+    func buttonPressed()
+}
+protocol ButtonDataSourceProtocol: ButtonPressedProtocol, ButtonTextProtocol{
+    
+}
+//protocol TextProtocol{
+//    var text:String { get }
+//}
+//class TextDataSource: TextProtocol {
+//    var text: String = "Hello world"
+//}
+//class TextDatasourceCustom: TextProtocol {
+//    var text: String = "Hello world"
+//}
+
 struct ProtocolBootCamp: View {
+    //let colorThem: DefaultColorThem = DefaultColorThem()
+    //let colorThem: AlternativeColorThem = AlternativeColorThem()
+    let colorThem: ColorThemProtocol
+    let datasource: ButtonDataSourceProtocol
     var body: some View {
-        Text(TextDataSource().text)
+        ZStack{
+            colorThem.tertiary
+                .ignoresSafeArea()
+            Text(datasource.buttonText)
+                .font(.headline)
+                .foregroundStyle(colorThem.secondary)
+                .padding()
+                .background(colorThem.primary)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .onTapGesture {
+                    datasource.buttonPressed()
+                }
+        }
     }
 }
 
 #Preview {
-    ProtocolBootCamp()
+    ProtocolBootCamp(colorThem: AnotherColorThem(), datasource: DefaultDataSource())
 }
